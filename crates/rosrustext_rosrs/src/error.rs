b9a387cb56ab9c3c3095f9bb41220eb22b92413d
ECM <ca99896@gmail.com>
2026-01-16 22:54:20 +0000

    UPdaet errors to use ros feature

--- crates/rosrustext_rosrs/src/error.rs
+++ crates/rosrustext_rosrs/src/error.rs
@@ -4,14 +4,18 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
+    #[cfg(feature = "ros2")]
     Rclrs(rclrs::RclrsError),
+
     Core(rosrustext_core::error::CoreError),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            #[cfg(feature = "ros2")]
             Error::Rclrs(e) => write!(f, "rclrs error: {e}"),
+
             Error::Core(e) => write!(f, "core error: {e}"),
         }
     }
@@ -20,12 +24,15 @@ impl fmt::Display for Error {
 impl StdError for Error {
     fn source(&self) -> Option<&(dyn StdError + 'static)> {
         match self {
+            #[cfg(feature = "ros2")]
             Error::Rclrs(e) => Some(e),
+
             Error::Core(e) => Some(e),
         }
     }
 }
 
+#[cfg(feature = "ros2")]
 impl From<rclrs::RclrsError> for Error {
     fn from(e: rclrs::RclrsError) -> Self {
         Error::Rclrs(e)
